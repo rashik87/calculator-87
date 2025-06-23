d9a44a87d3f5d8e912d4b0bc256fa332f93d5d66
rashik87 <rr7597372@gmail.com>
2025-06-23 08:20:01 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCC2W9TQWs_f0saCkpPyxX2fgk2g2nset4",
+  authDomain: "rashik-calculator.firebaseapp.com",
+  projectId: "rashik-calculator",
+  storageBucket: "rashik-calculator.firebasestorage.app",
+  messagingSenderId: "780558571965",
+  appId: "1:780558571965:web:84e7d9c702a8736f17dad4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
